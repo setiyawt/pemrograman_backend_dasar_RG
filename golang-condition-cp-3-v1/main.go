c6d9afdeed73a1b7b18201725f15df74156c2824
setiyawt <setiyawati324@gmail.com>
2024-03-14 22:01:56 +0700

    excercise condition-cp-3-v1

--- golang-condition-cp-3-v1/main.go
+++ golang-condition-cp-3-v1/main.go
@@ -2,11 +2,38 @@ package main
 
 import "fmt"
 
+var math, science, english, indonesia int
+
 func GetPredicate(math, science, english, indonesia int) string {
-	return "" // TODO: replace this
+	rata2 := (math + science + english + indonesia) / 4
+	if rata2 == 100 {
+		return "Sempurna"
+	} else if rata2 >= 90 && rata2 < 100 {
+		return "Sangat Baik"
+	} else if rata2 >= 80 && rata2 < 90 {
+		return "Baik"
+	} else if rata2 >= 70 && rata2 < 80 {
+		return "Cukup"
+	} else if rata2 >= 60 && rata2 < 70 {
+		return "Kurang"
+	} else if rata2 < 60 {
+		return "Sangat kurang"
+	} else {
+		return ""
+	}
 }
 
 // gunakan untuk melakukan debug
 func main() {
-	fmt.Println(GetPredicate(50, 80, 100, 60))
+	fmt.Print("math: ")
+	fmt.Scan(&math)
+	fmt.Print("science: ")
+	fmt.Scan(&science)
+	fmt.Print("english: ")
+	fmt.Scan(&english)
+	fmt.Print("indonesia: ")
+	fmt.Scan(&indonesia)
+	result := GetPredicate(math, science, english, indonesia)
+	fmt.Println(result)
+
 }
